b0b34f0bb17929085acf8c2b561b272026af30a9
Hajavololona Armellin <armelgeek5@gmail.com>
2025-01-08 23:09:01 +0300

    refactor: update Supabase configuration for improved security and clarity

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,8 +2,12 @@ import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rmnluqlmhbamftdvaxxb.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtbmx1cWxtaGJhbWZ0ZHZheHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4ODMyMjQsImV4cCI6MjA1MTQ1OTIyNH0.9u26exWroG5SHeM0M6VQLHoy62M7UbzigIIsASOee-Y'
+//const supabaseUrl = 'https://rmnluqlmhbamftdvaxxb.supabase.co'
+//const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtbmx1cWxtaGJhbWZ0ZHZheHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4ODMyMjQsImV4cCI6MjA1MTQ1OTIyNH0.9u26exWroG5SHeM0M6VQLHoy62M7UbzigIIsASOee-Y'
+
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE';
+const supabaseUrl = 'https://supabase.diactivplus.com';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
@@ -12,4 +16,4 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     detectSessionInUrl: false,
   },
-}) 
+})
